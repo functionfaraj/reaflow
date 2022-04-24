d3a8264da8e99bec35368c209f58f3407fc0f61f
faraj <function.faraj@gmail.com>
2022-04-24 20:38:36 +0300

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { nodes as defaultNodes, edges as defaultEdges } from "./data";
 export const App = () => {
   const [nodes, setNodes] = React.useState(defaultNodes);
   const [edges, setEdges] = React.useState(defaultEdges);
-  const getAllChildren = (source) => {};
   const nodeParent = React.useCallback(
     edges.reduce(function (r, a) {
       r[a.from] = r[a.from] || [];
@@ -13,16 +12,11 @@ export const App = () => {
     }, Object.create({})),
     [edges]
   );
-
-  console.log(nodeParent);
   const handleOnClickNode = (node) => {
     const nodeId = node.id;
-    console.log({ node });
     if (node.properties.collapse) {
-      console.log("collapse");
       //get all edges from old edges
       let fromEdges = defaultEdges.filter((elem) => elem.from === nodeId);
-      console.log("collapse fromEdges", fromEdges);
       const tempEdges = [...edges];
       for (const iterator of fromEdges) {
         tempEdges.push(iterator);
@@ -35,23 +29,17 @@ export const App = () => {
           }
         })
         .filter(Boolean);
-      console.log("collapse fromEdges to", fromEdges);
-
       ///get all nodes fromEdges to
       const retrivedNode = defaultNodes.filter((elem) => {
         if (fromEdges.includes(elem.id)) return true;
       });
-      console.log("collapse retrivedNode", retrivedNode);
       const tempNodes = [...nodes];
       for (const iterator of retrivedNode) {
         tempNodes.push(iterator);
       }
       const index = tempNodes.findIndex((elem) => {
-        console.log({ elem });
-        console.log({ nodeId });
         return elem.id === nodeId;
       });
-      console.log({ index });
       tempNodes[index] = {
         ...tempNodes[index],
         collapse: false,
@@ -67,29 +55,22 @@ export const App = () => {
           }
         })
         .filter(Boolean);
-      console.log({ fromEdges });
       for (const edge of fromEdges) {
         //CheckIfParent
         if (nodeParent[edge]) {
-          console.log("nodeParent" + edge, nodeParent[edge]);
           //CheckIfParent
           if (nodeParent[nodeParent[edge]]) {
             const parent = nodeParent[nodeParent[edge]];
-            console.log("nodeParent 2", parent);
             finalEdges = finalEdges.filter((elem) => {
               if (!parent.includes(elem.to)) return true;
             });
             finalNode = finalNode.filter((elem) => {
               if (!parent.includes(elem.id)) return true;
             });
-            console.log({finalEdges})
-            console.log({finalNode})
           }
-          console.log("nodeParent" + edge, nodeParent[edge]);
           finalEdges = finalEdges.filter((elem) => {
             if (!nodeParent[edge].includes(elem.to)) return true;
           });
-          console.log({ finalEdges });
           finalNode = finalNode.filter((elem) => {
             if (!nodeParent[edge].includes(elem.id)) return true;
           });
@@ -99,15 +80,9 @@ export const App = () => {
       finalNode = finalNode.filter((elem) => {
         if (!fromEdges.includes(elem.id)) return true;
       });
-      console.log({ nodes });
-      console.log({ finalNode });
-      console.log({ nodeId });
       const index = finalNode.findIndex((elem) => {
-        console.log({ elem });
-        console.log({ nodeId });
         return elem.id === nodeId;
       });
-      console.log({ index });
       finalNode[index] = {
         ...finalNode[index],
         collapse: true,
@@ -116,7 +91,6 @@ export const App = () => {
       finalEdges = finalEdges.filter((elem) => {
         if (!fromEdges.includes(elem.to)) return true;
       });
-      console.log({ finalEdges });
       setEdges(finalEdges);
     }
 
@@ -138,10 +112,7 @@ export const App = () => {
           <Node
             nodeProps={nodeProps}
             onClick={(e) => {
-              console.log("click", e.detail);
               if (e.detail === 2) {
-                console.log("Double click");
-                console.log(nodeProps);
                 handleOnClickNode(nodeProps);
               }
             }}
@@ -149,16 +120,6 @@ export const App = () => {
           />
         );
       }}
-      // edge={(edgeProps) => {
-      //   const edge = JSON.stringify(edgeProps);
-      //   const source = JSON.parse(edge).source;
-      //   if (isSourceExit(source)) {
-      //     return <Edge {...edgeProps} />;
-      //   } else {
-      //     return <></>;
-      //   }
-      // }}
-      onLayoutChange={(layout) => console.log("Layout", layout)}
     />
   );
 };
